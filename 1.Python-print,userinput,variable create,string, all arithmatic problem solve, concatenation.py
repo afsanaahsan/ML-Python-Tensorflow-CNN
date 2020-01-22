ea79d6356712bc2e2e478dcc020f96a9fffe7408
Afsana Ahsan Jeny <32925994+afsanaahsan@users.noreply.github.com>
2020-01-23 02:34:27 +0600

    Add files via upload

---
+++ 1.Python-print,userinput,variable create,string, all arithmatic problem solve, concatenation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+print("Hello World!")
+print(22//7)
+print(22*7)
+print("afsana"+" ahsan")
+a="roman"
+b=619
+print(a+str(b)) #python don't add different type data together
+
+
+# In[12]:
+
+
+var1=int(input("enter 1st num:"))
+var2=int(input("enter 2nd num:"))
+print(var1+var2)
+print(var1-var2)
+print(var1*var2)
+print(var1/var2)
+print(var1//var2)
+print(var1%var2)
+
+
+# In[ ]:
+
+
+
+
---
+++ 4.Control flow and Indentation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+for i in range(1,12):
+    print("value of I:"+str(i))
+    print("for loop complete") #this is called indentation 
+
+
+# In[4]:
+
+
+for var1 in range(1,12):
+    print("value of I:"+str(var1))
+print("for loop complete")  #this is called indentation 
+
+
+# In[5]:
+
+
+for var1 in range(1,12):
+    print("No. {} square is {} and cube is {}".format(var1,var1**2,var1**3))
+print("for loop complete")
+
+
+# In[ ]:
+
+
+
+

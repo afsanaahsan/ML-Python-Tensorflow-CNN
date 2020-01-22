e57cf4841cb904c2853401b97a93fb7ab37866b3
Afsana Ahsan Jeny <32925994+afsanaahsan@users.noreply.github.com>
2020-01-22 21:02:04 +0600

    Add files via upload

---
+++ Simple string print, format function,f function.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+var1="hello"
+var2="world"
+message1=var1+" "+var2+" "+'good morning'
+print(message1)
+
+
+# In[9]:
+
+
+message2='{} {} good morning'.format(var1,var2)
+print(message2)
+
+
+# In[10]:
+
+
+message3=f'{var1} {var2} good morning'
+print(message3)
+
+
+# In[ ]:
+
+
+
+
